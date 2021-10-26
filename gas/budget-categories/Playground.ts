9937118e2fc098fed2c0cbdadbf4e5ea36a32a4b
Thomas Malt <thomas@malt.no>
2021-10-26 08:11:59 +0200

    adding new functions

--- gas/budget-categories/Playground.ts
+++ gas/budget-categories/Playground.ts
@@ -2,3 +2,21 @@ function cubicBezier(x0: number, y0: number, x1: number, y1: number) {
   const foo = parseFloat('1.234');
   return true;
 }
+
+/**
+ * Returns a list of the dates left in month
+ *
+ * @returns Array
+ * @customfunction
+ */
+function daysLeftinMonth() {
+  // calculate dates left in month from today, return as array
+  const isLeap = (year: number) => (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+  const range = (start: number, stop: number) => Array.from({ length: stop - start }, (_, i) => start + i);
+
+  const today = new Date();
+  const months = [31, isLeap(today.getFullYear()) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  const date = today.getDate();
+
+  return range(today.getDate(), months[today.getMonth()] + 1);
+}
