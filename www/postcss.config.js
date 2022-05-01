ea99042a6b92e1154cf2fbe963b444f5d2ca503f
Thomas Malt <thomas@malt.no>
2022-05-01 16:19:03 +0200

    cleaning up repo

--- www/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const postcssPresetEnv = require('postcss-preset-env');
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = {
-    plugins: [postcssPresetEnv(), require('cssnano')]
-  };
-  return;
-}
-module.exports = {};
