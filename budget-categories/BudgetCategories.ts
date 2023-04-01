d9e79e144fd2ca086790103ebc036e93e89421f1
Thomas Malt <thomas@malt.no>
2023-04-01 13:51:51 +0200

    latest categories

--- budget-categories/BudgetCategories.ts
+++ budget-categories/BudgetCategories.ts
@@ -296,6 +296,7 @@ function category(options: CategoryOptions): string {
   if (desc.match(/nettbank til.*9801.61.97645/i)) return 'lommepenger';
   if (desc.match(/Til: Silje Isabelle/i)) return 'lommepenger';
   if (desc.match(/Til: Ella Lyche/i)) return 'lommepenger';
+  if (desc.match(/Til: Camilla Godø Krohn/i)) return 'lommepenger';
   if (desc.match(/pinger/i)) return 'lommepenger';
 
   // kiosk eller drivstoff
